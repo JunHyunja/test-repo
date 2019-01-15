e1e62744d4a6f3f7b713362f72b572dce51b8241
t12146@CORP.KOREACB.KR <t12146@CORP.KOREACB.KR>
2019-01-15 14:04:16 +0900

    vdskfasdfkafsd

---
+++ src/main/java/com/example/mymicroservice/Course.java
@@ -0,0 +1,46 @@
+package com.example.mymicroservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+public class Course {
+    @Id @GeneratedValue
+    Long id;
+
+    String title;
+    int minEnrollment;
+    int maxEnrollment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getMinEnrollment() {
+        return minEnrollment;
+    }
+
+    public void setMinEnrollment(int minEnrollment) {
+        this.minEnrollment = minEnrollment;
+    }
+
+    public int getMaxEnrollment() {
+        return maxEnrollment;
+    }
+
+    public void setMaxEnrollment(int maxEnrollment) {
+        this.maxEnrollment = maxEnrollment;
+    }
+}
